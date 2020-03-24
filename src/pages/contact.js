9715381bec0e4c6283119d48281325f0397fa167
lcsrbn <luca.sorbini@gmail.com>
2020-03-24 19:04:39 +0100

    added contatc form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,7 +10,7 @@ function ContactPage() {
       <section className="mb-8 flex m-auto sm:justify-center w-10/12 lg:w-2/3 md:w-2/3 sm:w-4/5">
         <form
             name="contact-form"
-            method="post"           
+            method="POST"           
             data-netlify="true"
             data-netlify-honeypot="bot-field" 
             action="/succes"
