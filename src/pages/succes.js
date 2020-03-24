5ac9cbfbede7d453e7c45bab672e7300da26ecb0
lcsrbn <luca.sorbini@gmail.com>
2020-03-24 17:40:20 +0100

    added contatc form

--- src/pages/succes.js
+++ src/pages/succes.js
@@ -10,7 +10,7 @@ function Success() {
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
         title="Contact"
       />
-      <section className="flex flex-wrap mx-auto items-center w-10/12 lg:w-2/3 md:w-2/3 sm:w-4/5 h-screen">
+      <section className="flex flex-wrap mx-auto items-center -mt-24 w-10/12 lg:w-2/3 md:w-2/3 sm:w-4/5 h-screen">
         <div className="w-full">
             <h2 className=" text-5xl underline font-bold text-gray-700">
                 Grazie per avermi contattato, ci sentiamo presto!
